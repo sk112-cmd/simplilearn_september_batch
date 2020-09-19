4c8d2f5c5e5a98536b1af2a6332ceeef2aeb4181
vinayingalahalli <vinay.ingalahalli1989@gmail.com>
2020-09-19 17:38:00 +0000

    19th september commit

---
+++ exceptions_demo/src/eg1/Demo1.java
@@ -0,0 +1,28 @@
+package eg1;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		int x=10;
+		int y=0;
+		int res=0;
+		
+		try {
+		res=x/y;
+		String s=null;
+		System.out.println(s.length());
+		}catch(ArithmeticException e) {
+			System.out.println("You cannot divide by zero.. Please re-enter");
+		}catch(NullPointerException e) {
+			System.out.println("Something is null there");
+		}
+		
+		finally {
+			System.out.println("Result is "+res);
+		}
+		System.out.println("This is other LOC in this app");
+
+	}
+
+}
